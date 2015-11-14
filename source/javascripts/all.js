e61e8202eca26150b91cc119484a689b5ccd50ff
Chuck Carpenter <chuck.carpenter@me.com>
2015-11-14 15:47:14 -0600

    add function to trophy a comment

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,15 @@
 //= require_tree .
+
+window.onload = function() {
+    var $comments = document.getElementsByClassName('m-comments__trophy'),
+        // converts NodeList to iterable array
+        commentsArray = Array.prototype.slice.call( $comments );
+
+    function trophyIt() {
+        this.className += ' active';
+    }
+
+    commentsArray.forEach( function( el ) {
+        el.addEventListener('click', trophyIt);
+    });
+}
