2886dc4dd88ce9c0a48133b787c432ee992813eb
Chuck Carpenter <chuck.carpenter@me.com>
2015-11-14 16:34:45 -0600

    add interactivity for trophy clicks

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,52 @@
-//= require_tree .
+/**
+ *
+ * This sets up after window onload() fires to listen
+ * when trophies are clicked and give UI feedback.
+ *
+ */
 
 window.onload = function() {
     var $comments = document.getElementsByClassName('m-comments__trophy'),
         // converts NodeList to iterable array
         commentsArray = Array.prototype.slice.call( $comments );
 
+    /**
+     * This looks at the element that triggers the event and will
+     * add an active class, if one doesn't exist.
+     *
+     * @returns {object} - clicked element modified className prop
+     */
     function trophyIt() {
-        this.className += ' active';
+        /**
+         * Find the counter element for the trophy that was clicked
+         *
+         * @param {object} parentEl - pointer reference for 'this'
+         * @returns {object} clickedCounter - element to be changed
+         */
+        function counterEl( parentEl ) {
+            for ( var i = 0; i < parentEl.children.length; i++ ) {
+                if ( parentEl.children[i].className === 'm-comments__trophy-count' ) {
+                    var clickedCounter = parentEl.children[i];
+
+                    return clickedCounter;
+                }
+            }
+        }
+
+        var clicked = counterEl( this ),
+        counter = parseInt(clicked.innerHTML, 10);
+
+        if ( this.classList.contains('active') ) {
+            counter--;
+            clicked.innerHTML = counter;
+
+            return this.className = this.className.replace( ' active', ' ' );
+        }
+
+        counter++;
+        clicked.innerHTML = counter;
+
+        return this.className += ' active';
     }
 
     commentsArray.forEach( function( el ) {
